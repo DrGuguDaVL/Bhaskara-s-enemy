3758a5d715e93ffb44b06fd4b2f8531f09a407d0
DrGuguDaVL <gustavo.vilela.veiga@escola.pr.gov.br>
2025-09-10 11:14:04 -0300

    4

--- file.js
+++ file.js
@@ -1,7 +1,1 @@
-const output = document.getElementById("output");
-
-for (let i = 1; i <= 10; i++) {
-    const line = document.createElement("div");
-    line.textContent = i;
-    output.appendChild(line);
-}
+<
