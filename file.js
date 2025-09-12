8d3d4de74ae0c226681e7726c698f7dd27acd60f
DrGuguDaVL <gustavo.vilela.veiga@escola.pr.gov.br>
2025-09-11 22:47:21 -0300

    fr

--- file.js
+++ file.js
@@ -1,1 +1,9 @@
-<
+ const input = document.getElementById("colorInput");
+
+  input.addEventListener("input", function () {
+    // Get the color typed in
+    const color = input.value;
+
+    // Try to set the background color of the wrapper
+    document.getElementById("output").style.backgroundColor = color;
+  });
